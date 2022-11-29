1f226e2093b7b13562d481e11505638a434e3d0a
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-11-29 18:32:31 +0100

    Tag 1 fertig

---
+++ CSharpGrundlagenkurs/Modul004_Schleifen_Enums/Program.cs
@@ -0,0 +1,219 @@
+﻿namespace Modul004_Schleifen_Enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Schleifen und Zählen
+
+            int a = 0;
+            int b = 10;
+
+
+            //Kopfgesteuerte Schleife -> Bedeutet: erst die Prüfung, dann der auszuführende Code-Block
+
+            while (a < b) //Wenn a den Wert 10 erreicht, wird die Schleife nicht mehr ausgeführt 
+            {
+                Console.WriteLine($"A ist kleiner als B! (Wert von a: {a})");
+                a++;
+            }
+
+            a = -45;
+
+            //DO-WHILE-Schleife (Fussgesteuerte Schleife) 
+            ///Auch die DO-WHILE-Schleife wird wiederholt, solange die Bedingung wahr ist. Allerdings wird die Bedingung erst am Schleifen_
+            ///ende geprüft, weshalb die Schleife mindestens einmal durchläuft.
+            do
+            {
+                Console.WriteLine(a);
+
+                a = a + 1; // a++ -> erhöht um den Wert 1
+            } while (a <  0);
+
+
+
+            //Wenn Variablen inkrementiert oder dekrementiert werden müssen, ist eine for-Schleife die bessere Wahl
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(i);
+
+            for (int i = -45; i < 0; i++)
+                Console.WriteLine(i);
+            #endregion
+
+            #region Endlosschleifen
+            a = 0;
+            b = 10;
+
+
+            //Endlossschleife
+
+            while (true) //Condition ist immer true 
+            {
+                a++;
+                Console.WriteLine(a);
+
+
+                //Wie kommen wir aus der Schleife herauf
+
+                if (a > b)
+                    break; // Ich springe aus der Schleife heraus
+            }
+
+
+
+
+            a = 0;
+            b = 100; 
+            while (true) //Condition ist immer true 
+            {
+                a++;
+                //Ist das Ende der Schleife erreich? 
+
+                if (a > b)
+                    break; // Ich springe aus der Schleife heraus
+
+                //Console.WriteLine(a);
+
+                if (a % 2 != 0)
+                    continue; //Bei einer ungeraden Zahl, wird der nächste Schleifen-Intervall angegangen
+
+                Console.WriteLine($"Gerade Zahlen {a}");
+            }
+
+            //Endlosschleife mit for 
+            //Eine einfache for (a = 0; a < b; a++) ist viel besser
+            a = 0;
+            b = 100;
+            for (;true;)
+            {
+                a++;
+                //Ist das Ende der Schleife erreich? 
+
+                if (a > b)
+                    break; // Ich springe aus der Schleife heraus
+
+                //Console.WriteLine(a);
+
+                if (a % 2 != 0)
+                    continue; //Bei einer ungeraden Zahl, wird der nächste Schleifen-Intervall angegangen
+
+                Console.WriteLine($"For-Schleife -> Gerade Zahlen {a}");
+            }
+
+            #endregion
+
+            #region for-Schleife vs. foreach-Schleife 
+            int[] zahlen = { 2, 3, 5, 4 };
+
+            //Iteration über ein Array mittels For-Schleife
+            for (int i = 0; i < zahlen.Length; i+=2) //ich muss nicht unbedingt mit den Wert 1 erhöhen sondern ehöhen um den Wert 2
+            {
+                Console.WriteLine(i);
+            }
+
+
+            
+
+            //FOREACH-Schleifen können über Collections laufen und sprechen dabei jedes Element genau einmal an
+            foreach (int currentZahl in zahlen)
+            {
+                Console.WriteLine(currentZahl);
+            }
+
+
+
+
+            #endregion
+
+
+
+            #region Enums
+            Wochentag wochentag = Wochentag.Montag; // Montag wird zugewiesen 
+
+            if (wochentag == Wochentag.Freitag)
+                Console.WriteLine("Es ist Wochenende");
+
+            #endregion
+
+            #region Warum werden Enums verwendet
+
+            string[] wochentage = new string[] { "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag" };
+            string currrentWochentag = wochentage[4]; //unleserlicher als bei Enums
+
+            if (currrentWochentag == wochentage[4])
+                Console.WriteLine("Wochenende");
+            #endregion
+
+            //Deklaration und Initialisierung einer Variablen vom Enumerator-Typ
+            Wochentag heutigerTag = Wochentag.Dienstag;
+            Console.WriteLine($"Heute ist also {heutigerTag}.");
+
+            Console.WriteLine("Welcher Wochentag is dein Lieblingstag?");
+
+            for (int i = 1; i < 8; i++)
+            {
+                Console.WriteLine($"{i}: {(Wochentag)i}");
+            }
+
+            //Speichere einer Benutzereingabe (Int) als Enumerator
+            heutigerTag = (Wochentag)int.Parse(Console.ReadLine());
+            Console.WriteLine($"Dein Lieblingstag ist also {heutigerTag}.");
+
+            //Parsen eines String in ein Enum
+
+            heutigerTag = (Wochentag)Enum.Parse(typeof(Wochentag), "Mittwoch");
+
+
+            //Warum verwendet man ein Switch z.B bei Enums
+            if (heutigerTag == Wochentag.Montag)
+            {
+
+            }
+            else if (heutigerTag == Wochentag.Dienstag)
+            {
+
+            }
+            else if(heutigerTag == Wochentag.Mittwoch)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            //Switch arbeitet schneller als if else 
+
+            //SWITCHs sind eine verkürzte Schreibweise für IF-ELSE-Blöcke. Mögliche Zustände der übergebenen Variablen werden 
+            //in den CASES definiert
+
+            switch(heutigerTag)
+            {
+                case Wochentag.Montag:
+                    //Wenn Montag, dann.... bis break
+                    Console.WriteLine("Wochenstart");
+                    break; //Springe aus dem Switch-Statement heraus
+                case Wochentag.Dienstag:
+                case Wochentag.Mittwoch:
+                case Wochentag.Donnerstag:
+                    Console.WriteLine("Normale Woche"); //Bei Di, Mi, Do
+                    Console.WriteLine("Di/Mi oder Donnerstag");
+                    break;
+                //case Wochentag.Freitag:
+                //case Wochentag.Samstag:
+                //case Wochentag.Sonntag:
+                    //Console.WriteLine("Wochenende");
+                    //break;
+                default:
+                    Console.WriteLine("Wochenende");
+                    break; 
+
+            }
+
+
+
+        }
+    }
+
+    enum Wochentag { Montag=1, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag }
+}
---
+++ CSharpGrundlagenkurs/Modul005_Funktionen/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Modul005_Funktionen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 11;
+            int b = 12;
+
+            //Methodenaufruf mit Parameter und Rückgabewert
+            int result = Addieren(a, b); 
+            Console.WriteLine(result); //23
+
+
+            double c = 11.4;
+            double d = 23.7;
+            //Überladung einer Methode 
+            double resultB =  Addieren(c, d);
+            Console.WriteLine(resultB);
+
+            //Verwenden von Params
+            int erg = BildeSumme(2, 5, 4, 3, 6, 3, 2, 6, 34, 3, 4, 6, 3, 2222, 44);
+
+
+            //Verwenden ohne Params 
+            int[] array = { 1, 4, 6, 34, 21 };
+            erg = BildeSummeWithoutParams(array);
+
+
+            erg = Subtrahieren(22, 11);//c = 0 und d = 0
+            erg = Subtrahieren(33, 1, 4); // d = 0
+            erg = Subtrahieren(100, 2, 3, 5); //c und d haben expliziet einen Wert zugewiesen bekommen 
+
+        }
+
+
+        //Jede Funktion/Methode besteht aus einem Kopf und einem Körper
+        ///Der Kopf besteht aus den MODIFIERN (public static), dem RÜCKGABEWERT (int), dem NAMEN (Addiere) sowie den ÜBERGABEPARAMETERN <summary>
+        /// Der Kopf besteht aus den MODIFIERN (public static), dem RÜCKGABEWERT (int), dem NAMEN (Addiere) sowie den ÜBERGABEPARAMETERN
+        
+        /// </summary>
+        /// <param name="a">Parameter 1</param>
+        /// <param name="b">Parameter 2</param>
+        /// <returns>Summe</returns>
+        static int Addieren(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Addieren(int a, int b, int c)
+            => Addieren(a, b) + c;
+
+        static double Addieren(double a, double b)
+        {
+            return a + b;
+        }
+
+
+        //Params wird selten verwenden
+
+        //Das PARAMS-Stichwort erlaubt die Übergabe einer beliebige Anzahl von gleichartigen Daten, welche innerhalb
+        //der Methode als Array interpretiert werden
+        static int BildeSumme(params int[] summanden)
+        {
+            int summe = 0;
+
+            foreach (int currentSummanden in summanden)
+            {
+                summe = summe + currentSummanden; //summe += currentSummanden;
+            }
+
+            //Best Practice
+            //int result = summanden.Sum();
+
+            return summe;
+        }
+
+        static int BildeSummeWithoutParams(int[] summanden)
+            => summanden.Sum();
+
+
+
+        ///Wird einem Parameter mittels =-Zeichen ein Defaultwert zugewiesen wird dieser Parameter OPTIONAL und muss bei Aufruf nicht zwangs-
+        ///läufig mitgegeben werden. OPTIONALE Parameter müssen am Ende der Parameter stehen.
+        static int Subtrahieren(int a, int b, int c=0, int d = 0)
+        {
+            return a - b - c - d;
+        }
+
+    }
+}
