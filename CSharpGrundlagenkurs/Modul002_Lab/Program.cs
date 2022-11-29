70ca058d6242861d074d0f3a3f59b670e15e9dc4
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-11-29 13:20:48 +0100

    Lab fertig

---
+++ CSharpGrundlagenkurs/Modul002_Lab/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Modul002_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entfernungInMEtern, stunden, minuten, sekunden;
+
+            double meterProSekunde, kilometerProStunde, meilenProStunde;
+
+
+            #region Eingabe
+            Console.WriteLine("Bitte gib folgende Angaben ein:");
+            Console.Write("Entfernung in Meter: ");
+
+            entfernungInMEtern = int.Parse(Console.ReadLine());
+
+            Console.Write("Stunden: ");
+            stunden = int.Parse(Console.ReadLine());
+
+            Console.Write("Minuten ");
+            minuten= int.Parse(Console.ReadLine());
+
+            Console.Write("Sekunden ");
+            sekunden = int.Parse(Console.ReadLine());
+            #endregion
+
+            Console.WriteLine();
+
+            sekunden = sekunden + (minuten * 60) + (stunden * 3600);
+            meterProSekunde = entfernungInMEtern / (double)sekunden;
+
+            kilometerProStunde = meterProSekunde * 3.6;
+
+            meilenProStunde = kilometerProStunde * 0.62137119;
+
+
+
+
+            Console.WriteLine($"Meter/Sekunde: \t\t {Math.Round(meterProSekunde, 2)}");
+            Console.WriteLine($"Kilometer/Stunde: \t\t {Math.Round(kilometerProStunde, 2)}");
+            Console.WriteLine($"Meilen/Stunde: \t\t {Math.Round(meilenProStunde, 2)}");
+
+
+
+        }
+    }
+}
--- CSharpGrundlagenkurs/Modul002_Variablen_Datentypen_Konventierung_Operatoren/Program.cs
+++ CSharpGrundlagenkurs/Modul002_Variablen_Datentypen_Konventierung_Operatoren/Program.cs
@@ -1,5 +1,6 @@
 ﻿//
 using System;
+using System.Net.Http.Headers;
 
 namespace Modul002_Variablen_Datentypen_Konventierung_Operatoren
 {
@@ -51,6 +52,7 @@ namespace Modul002_Variablen_Datentypen_Konventierung_Operatoren
 
             #endregion
 
+            #region Ausgabe von Strings
             //SO bitte nicht
             string term = "Ich bin " + alter + " Jahre alt und wohne in " + stadt + ".";
             Console.WriteLine(term);
@@ -63,13 +65,103 @@ namespace Modul002_Variablen_Datentypen_Konventierung_Operatoren
             //$-Operator (Variablen werden direkt in {} - Klammern geschrieben 
             term = $"Ich bin {alter} Jahre alt und wohne in {stadt}.";
             Console.WriteLine(term);
-
             Console.WriteLine($"Ich bin jetzt {alter *2} und wohne in {stadt}.");
 
+            int a = 55;
+            int b = 15;
+            Console.WriteLine($"{a} + {b} = {a+b}");
 
+            //String-Formatierung mittels Escape-Sequenzen  -> https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/strings/
 
+            string bsp = "Dies ist ein \tTabulator und dies ist ein \n Zeilenumbruch";
+            Console.WriteLine(bsp);
             //Programm stoppt, bis eine Tastatureingabe erfolgt 
-            Console.ReadKey(); 
+
+
+            string theOldPathStyle = "C:\\Windows\\Temp";
+
+            string bestPracticePath = @"C:\Windows\Temp"; //intern -> C:\\Windows\\Temp
+
+            string verbatim = $@" Dies ist ein    Tabulator und dies ein
+Zeilenumbruch {stadt}";
+
+            Console.WriteLine(verbatim);
+
+            #endregion
+
+
+            #region Eingabe eines Strings
+
+            Console.Write("Bitte gib einen Namen ein: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+
+            #endregion
+
+            #region Eingabe einer Zahl + Umwandlung in Integer
+
+            Console.WriteLine("Bitte gib deine Lieblingszahl ein:");
+            string zahlAlsString = Console.ReadLine();
+
+
+            //Variante 1 (älteste Schreibweise in .NET) String-Zahl -> wird nummerische Zahl (int)
+
+            int zahl;
+
+            //zahl = Convert.ToInt32(zahlAlsString); 
+
+            //Variante 2 (
+            //zahl = int.Parse(zahlAlsString);
+
+
+
+            #region Zusatz -> int.TryParse
+            //TryParse gibt an, ob das Konventieren erfolgreich war 
+            if (int.TryParse(zahlAlsString, out zahl))
+            {
+                //Erfolgreich
+                Console.WriteLine(zahl);
+            }
+            else
+            {
+                //Eingabefehler
+                Console.WriteLine($"Konventierung hat nicht geklappt -> Eingabe war {zahlAlsString}");
+            }
+
+
+            #endregion
+
+            #region Zahl -> String
+            //Implziete Konventierung -> WriteLine wandelt automatisch um
+            Console.WriteLine(zahl);
+
+            //Zahl wird in ein String mithilfe der Methode ToString() konventiert
+            zahlAlsString = zahl.ToString();
+            #endregion
+
+
+            #region Casten von Variablen
+
+            int intZahl = 33;
+
+            //Implizietes Casten
+            double doubleZahl = intZahl; //33.0000
+            doubleZahl = 45.51;
+
+            //Explizietes Casten (müssen Zieldatentyp angeben) 
+            intZahl = (int)doubleZahl; //45
+
+            #endregion
+
+
+            #endregion
+
+
+
+
+
+            Console.ReadKey();
+
         }
     }
 }
